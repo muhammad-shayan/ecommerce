1fba9cc43fa24bf7f33e0f84623fbd76e6847460
muhammad-shayan <shayanksa@gmail.com>
2023-09-21 20:17:14 +0500

    deployment to github

--- src/components/adminOnlyRoutes/AdminOnlyRoutes.jsx
+++ src/components/adminOnlyRoutes/AdminOnlyRoutes.jsx
@@ -1,38 +1,32 @@
-import { useSelector } from "react-redux"
-import { selectEmail } from "../../features/authSlice"
-import { Link } from "react-router-dom"
+import { useSelector } from "react-redux";
+import { selectEmail } from "../../features/authSlice";
+import { Link } from "react-router-dom";
 
-export const AdminOnlyRoutes = ({children}) => {
-    const email = useSelector(selectEmail)
-    if(email==='test@gmail.com'){
-        return children
-    }
-    else{
-        return (
-            <section style={{height:'80vh'}}>
-                <div className="container">
-                    <h2>Permission Denied</h2>
-                    <p>This page can only be viewed by an Admin user</p>
-                    <br />
-                    <Link to="/">
-                        <button className="--btn --btn-primary">&larr; Go Back</button>
-                    </Link>
-                </div>
-            </section>
-          )
-    }
-  
-}
+export const AdminOnlyRoutes = ({ children }) => {
+  const email = useSelector(selectEmail);
+  if (email === "shayanksa@gmail.com") {
+    return children;
+  } else {
+    return (
+      <section style={{ height: "80vh" }}>
+        <div className="container">
+          <h2>Permission Denied</h2>
+          <p>This page can only be viewed by an Admin user</p>
+          <br />
+          <Link to="/">
+            <button className="--btn --btn-primary">&larr; Go Back</button>
+          </Link>
+        </div>
+      </section>
+    );
+  }
+};
 
-
-export const AdminOnlyLinks = ({children}) => {
-    const email = useSelector(selectEmail)
-    if(email==='test@gmail.com'){
-        return children
-    }
-    else{
-        return null
-    }
-}
-
-
+export const AdminOnlyLinks = ({ children }) => {
+  const email = useSelector(selectEmail);
+  if (email === "shayanksa@gmail.com") {
+    return children;
+  } else {
+    return null;
+  }
+};
